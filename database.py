fe59736170932b0a0d3e61b405059a465026606e
SAHIL THAKUR <sahilthakur4528@gmail.com>
2026-07-26 23:20:17 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,17 +1,16 @@
-import pymysql
+import os
+import mysql.connector
 
 def get_connection():
     try:
-        connection = pymysql.connect(
-            host="localhost",
-            user="root",
-            password="sahil45",
-            database="smart_farming_db",
-            cursorclass=pymysql.cursors.DictCursor
+        conn = mysql.connector.connect(
+            host=os.environ.get("sql8.freesqldatabase.com"),
+            user=os.environ.get("sql8833866"),
+            password=os.environ.get("7ngE1JEQJN"),
+            database=os.environ.get("smart_farming_db"),
+            port=int(os.environ.get("MYSQL_PORT", 3306))
         )
-
-        return connection
-
+        return conn
     except Exception as e:
         print("Database Connection Error:", e)
         return None
