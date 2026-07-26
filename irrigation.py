698c90867cc70f0c01840cb844dd47178ce45fa9
SAHIL THAKUR <sahilthakur4528@gmail.com>
2026-07-26 23:22:48 +0530

    Update irrigation.py

--- irrigation.py
+++ irrigation.py
@@ -1,1 +1,16 @@
+import pandas as pd
 
+def irrigation_advice(crop, rainfall):
+
+    df = pd.read_csv("irrigation_dataset.csv")
+
+    result = df[
+        (df["Crop"] == crop) &
+        (df["Rainfall_Min"] <= rainfall) &
+        (df["Rainfall_Max"] >= rainfall)
+    ]
+
+    if not result.empty:
+        return result.iloc[0]["Advice"]
+
+    return "No Irrigation Advice Available"
